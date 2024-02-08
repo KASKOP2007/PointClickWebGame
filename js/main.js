57f1ec0e99a2aa2fa56643659fdd03e7fdef481a
KASKOP2007 <j.de.leeuw@sintlucasedu.nl>
2024-02-08 12:56:36 +0100

    2

--- js/main.js
+++ js/main.js
@@ -1,2 +1,48 @@
 document.getElementById("mainTitle").innerText = "Point and click adventure game"
 
+const gameWindow = document.getElementById("gameWindow");
+
+const mainCharacter = document.getElementById("mainCharacter")
+const offsetCharacter = 16;
+
+const door1 = document.getElementById("door1")
+
+gameWindow.onclick = function (e) {
+    var rect = gameWindow.getBoundingClientRect();
+    var x = e.clientX - rect.left
+    var y = e.clientY - rect.top
+
+    console.log(e.target.id);
+    mainCharacter.style.left = x - offsetCharacter + "px";
+    mainCharacter.style.top = y - offsetCharacter + "px";
+
+
+    switch (e.target.id) {
+        case "door1":
+            mainCharacter.style.backgroundColor = "#FFFF00"
+            door1.style.opacity = 0.5;
+            sign.style.opacity = 0.5;
+            break;
+        case "sign":
+            mainCharacter.style.backgroundColor = "#FFFF00"
+            door1.style.opacity = 0.5;
+            sign.style.opacity = 0.5;
+            break;
+
+
+        default:
+            //explode
+            mainCharacter.style.backgroundColor = "#442c1c"
+            door1.style.opacity = 1;
+            sign.style.opacity = 1;
+            break;
+
+    }
+
+    if (e.target.id == "door1") {
+        mainCharacter.style.backgroundColor = "#FFFF00"
+        door1.style.opacity = 0.5;
+    } else {
+
+    }
+}
